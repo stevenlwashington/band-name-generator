c6c1a28010147d9562816efee869cdbf4206464b
Steven Washington <stevenlwashington@gmail.com>
2015-10-02 18:32:45 -0700

    updated script.js

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000; // process.env reads environment variables. looks at enviro and sets port to 3000
 
-app.use(express.static(__dirname + "/app/")); //app.use says filter everything through this before it touches any other endpoints. static allows me to serve static files. __dirname is  ashortcut to my current directory
+app.use(express.static(__dirname + "/app/")); //app.use says filter everything through this before it touches any other endpoints. static allows me to serve static files. __dirname is  a shortcut to my current directory. essentially, this tells express that it will be serving files from the /app folder
 
 function Adjective () {
   this.happy = true;
   this.grumpy = true;
   this.tall = true;
   this.silly = true;
-}
-
+};
 var adjective = new Adjective();
 
 function getRandomWord (object) {
